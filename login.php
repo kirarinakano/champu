11cf96df9dfcc31f0e0f350a5ba3b3d8065d6e17
tomoki1996 <39045013+tomoki1996@users.noreply.github.com>
2018-05-10 16:38:53 +0800

    update

--- login.php
+++ login.php
@@ -1,27 +1,27 @@
 <?php
- include 'insert.php';
+ include 'connect.php';
 
 $Password = $_POST['Password'];
 $UserName = $_POST['UserName'];
 
- $sql = "SELECT * FROM  WHERE UserName = '$UserName' AND Password = '$Password'";
+ $sql = "SELECT * FROM userinfo WHERE UserName = '$UserName' AND Password = '$Password'";
  $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  echo "Welcome back " . $UserName . "<br><a href='login.php'>logout</a><br><a href='main.html'>enter</a>";
+  header('Location: additem.html');
 } else {
   echo "Error: Your username or/and password are wrong.";
 }
 
 
-function setConfirmMessage(confirm_password) {
- var password = document.getElementById("password").value;
- var message = "";
- if (password == confirm_password) {
-   message = "";
- } else {
-   message =  "please input same characters in both password form";
- }
+//function setConfirmMessage(confirm_password) {
+ //var password = document.getElementById("password").value;
+ //var message = "";
+ //if (password == confirm_password) {
+   //message = "";
+ //} else {
+   //message =  "please input same characters in both password form";
+ //}
 
 
 
--- register.php
+++ register.php
@@ -1,37 +1,25 @@
 <?php
 include 'connect.php';
 
-$Password = $_POST['Password'];
+$Password = $_POST["Password"];
 $Emailaddress = $_POST['Emailaddress'];
 $UserName = $_POST['UserName'];
 
 
-$sql = "INSERT INTO userinfo (UserName, Emailaddress, Password)
-VALUES ('$UserName', '$Emailaddrss' '$Password')";
+$sql = "INSERT INTO userinfo (userID, Username, Emailaddress, Password)
+VALUES ('','$UserName', '$Emailaddress', '$Password')";
 
 if ($conn->query($sql)) {
-  echo "New record created successfully<br><a href='login.html'>login</a>";
+  header('Location: login.html');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 
-    $pages    = mb_convert_kana($_POST['pages'], "ans", "utf-8");
+    //$pages    = mb_convert_kana($_POST['pages'], "ans", "utf-8");
 ?>
 <?php
-function setConfirmMessage(confirm_password) {
- var password = document.getElementById("password").value;
- var message = "";
- if (password == confirm_password) {
-   message = "";
- } else {
-   message =  "please input same characters in both password form";
- }
 
- var div = document.getElementById("pass_confirm_message");
- if (!div.hasFistChild) {div.appendChild(document.createTextNode(""));}
- div.firstChild.data = message;
-}
 ?>
 
 
