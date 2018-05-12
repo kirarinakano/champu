6cbf03c1553417dd264bec91d98f0fa1a939f68f
shumpei <syky09012568183@gmail.com>
2018-05-12 12:40:56 +0900

    added add and edit user function

---
+++ saveedit.php
@@ -0,0 +1,17 @@
+<?php
+include 'champuconnect.php';
+
+$Itemename = $_POST["Itemename"];
+$Amount = $_POST["amount"];
+
+$sql= "UPDATE champu SET Itemename='$itemname', Amount='$amount' WHERE infoID=$infoID";
+ 
+if ($conn ->query($sql)=== TRUE) {
+  echo "record is updated successfully";
+}else {
+  echo "Error during updating record:" . $conn->error;
+}
+
+  ?>
+
+<a href="home.php">HOME</a>
