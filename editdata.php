f088624b696ef5bd7c4d238c329bb2f1b4d1abb7
kirarinakano <kirari624@gmail.com>
2018-06-07 11:45:41 +0900

    third commint

--- editdata.php
+++ editdata.php
@@ -1,19 +1,112 @@
 <?php
+session_start();
 include 'connect.php';
 
-$item = $_POST["Itemname"];
-$amount = $_POST["amount"];
+$userID = $_SESSION["userID"];
+$itemID = $_SESSION["itemID"];
+$dataID = $_SESSION["dataID"];
 
+$errorMessage = "";
+$errorMessages = "";
 
-$sql ="INSERT INTO itemdata (Itemname, Amount)
-  VALUES ('$item', '$amount') ";
+$sql = "SELECT * FROM itemdata WHERE userID='$userID'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    $itemname = $row['Itemname'];
+    $amount = $row['Amount'];
 
-if($conn->query($sql) ===TRUE){
-  echo "New record created successfully";
-}else{
-  echo "Error:" .$sql."<br>".$conn->error;
+    $sql2 = "SELECT * FROM itemadddata WHERE Registerday = (SELECT MAX(Registerday) FRoM itemadddata WHERE itemID='$itemID')";
+    $result1 = $conn->query($sql2);
+      if ($result1->num_rows > 0) {
+        while ($row1 = $result1->fetch_assoc()) {
+        	$startday = $row1['Startday'];
+        	$endday = $row1['Endday'];
+        }
+      }
+  }
+}
+
+if (isset($_POST["update"])) {
+	$Itemname = $_POST['itemname'];
+	$Amount = $_POST['amount'];
+	$Startday = $_POST['startday'];
+	$Endday = $_POST['endday'];
+
+	$today = date("Y/m/d");
+
+	 if (strtotime($Endday) < strtotime($Startday)) {
+      $errorMessages = "Please select end day after start day you selected";
+     } elseif (strtotime($today) < strtotime($Endday)) {
+      $errorMessages = "Please select the date before the current date.";
+     } elseif (strtotime($today) < strtotime($Startday)) {
+      $errorMessage = "The start day should be before or today. Please reselect.";
+     } elseif (strtotime($Startday) > strtotime($Endday)) {
+     	$errorMessage = "The startday should be before the endday. Please reselect";
+     } else {
+     	$sql = "UPDATE itemdata SET Itemname='$Itemname',Amount='$Amount'";
+     	if ($conn->query($sql) === TRUE) {
+     	 echo "";
+    	} else {
+      	 echo "1 Error during updating record: " . $conn->error . "<br>";
+       	}
+
+       	$sql1 = "UPDATE itemadddata SET Startday='$Startday',Endday='$Endday' WHERE dataID='$dataID'";
+       	if ($conn->query($sql1) === TRUE) {
+     	 header('Location: main.php');
+    	} else {
+      	 echo "1 Error during updating record: " . $conn->error . "<br>";
+       	}
+     }
 }
 
 ?>
-<br><br><br>
-<a href="champu.html">HOME</a>
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="editdata.css">
+  <title>Edit Data</title>
+</head>
+<body>
+	<div class="header">
+      <div class="logo">
+        <img src="image/champulogo.png" width="80px" height="50px" alt="champulogo">
+         <p>CHAMPU</p>
+    
+     </div>
+  </div>
+  <div class="tittle">
+    <p>Edit Data</p>
+  </div>
+  <form action="editdata.php" method="POST" >
+    <div class="subtittle">
+       <p>Item name</p>
+       <p>Amout</p>
+    </div>
+    <div class="itemdata">
+      <input type="text" class="itemname" name="itemname" value="<?php echo $itemname ?>" maxlength="20" minlength="1" required placeholder="1-20" autocomplete="off">
+      <input type="number" class="amount" name="amount" value="<?php echo $amount ?>" min="1" required autocomplete="off">
+      <span>ml</span>
+    </div>
+    <div class="subtittle">
+      <p>Use start date</p>
+    </div>
+    <input type="date" value="<?php echo $startday ?>" name="startday" class="date" required>
+        <?php 
+        echo $errorMessage;
+        ?>
+    <div class="subtittle">
+      <p>Use end date</p>
+    </div>
+    <input type="date" value="<?php echo $endday ?>" name="endday" class="date" required>
+        <?php 
+        echo $errorMessages;
+        ?>
+    <div class="wrap_update">
+      <button type="submit" name="update" class="update">Update</button><br>
+    </div>
+    <div class="wrap_link">
+    <a style= 'color: #6ba3ff;' href='main.php'  class='link' >Back to main</a></div>
+  </form>
+</body>
+</html>
