35200c706b05ce2a7e1d1bbedc1be3f5a49cd152
kirarinakano <kirari624@gmail.com>
2018-06-11 18:10:14 +0900

    second commit

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// include 'connection.php';
-
-// if(!isset($_SESSION['access_token'])){
-// 	echo "<a href='$url'><img src='twitter-login-blue.png' style='margin-left:4%; margin-top: 4%'></a>";
-// } else {
-// 	header("location: user.php");
-// }
