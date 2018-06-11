c054053ec5de4e3594e1184eebe368483f08d03e
kirarinakano <kirari624@gmail.com>
2018-06-11 18:21:56 +0900

    third commit

--- connection.php
+++ connection.php
@@ -15,6 +15,10 @@ if (!isset($_SESSION['access_token'])) {
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$user = $connection->get("account/verify_credentials", ['include_email' => 'true']);
+	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+	$_SESSION['oauth_token'] = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
 //    $user1 = $connection->get("https://api.twitter.com/1.1/account/verify_credentials.json", ['include_email' => true]);
     // echo $user->email;echo "<br>";
 
