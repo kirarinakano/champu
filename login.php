2f4f7b50690af63b47bbb63c91518b156ce3e701
tanaka tomoki <tanakatomoki@tanaka-no-MacBook-Air.local>
2018-06-18 14:22:41 +0800

    login

--- login.php
+++ login.php
@@ -50,9 +50,14 @@ if (isset($_POST["submit"])){
 
 </head>
 <body>
-    <div class="top">
-       <img src="image/champulogo.png" width="80px" height="50px" alt="champulogo">
-    </div>
+   <div class="top" width="100px">
+     <div class="middle">
+       <img src="image/champulogo.png" width="80px" height="50px" align="left" class="img">
+     </div>
+     <div class="company">
+      <h2 class="logo" style="margin-top: 0px;"> CHAMPU</h2> 
+     </div>
+   </div>
 
       <p class="title">Log in</p><br>
         <div class="biggest">
