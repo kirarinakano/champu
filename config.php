ce25caf4997b3303368b95bd4be2d959f98ed525
kirarinakano <kirari624@gmail.com>
2018-06-19 10:45:45 +0900

    change config

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ session_start();
 require 'autoload.php';
 define('CONSUMER_KEY', 'xc6qkyKKqKqEfGekvHlflAp1s');
 define('CONSUMER_SECRET', 'cYNKe6BRLkQiJ5KSb3DPfV1LPJTssV0W2imWXFuoH7vhX4FLnf'); 												
-define('OAUTH_CALLBACK', 'http://localhost/twitter_login/callback.php');
+define('OAUTH_CALLBACK', 'http://localhost/champu/callback.php');
